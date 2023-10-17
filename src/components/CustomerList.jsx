a51f05ea7976a68af0d25a131d34c2dd7a6ca924
Vivekanand P V <vivekanandpv@outlook.com>
2023-10-17 16:04:08 +0530

    task-6 completed

--- src/components/CustomerList.jsx
+++ src/components/CustomerList.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import customerList from '../data.json';
 
 const CustomerList = (props) => {
+  const [customers, setCustomers] = React.useState([]);
+
+  React.useEffect(() => {
+    setCustomers((_) => customerList);
+  }, []);
+
   return (
     <>
-      <h4>Customer List</h4>
+      <h4>Customers</h4>
+      <table className='table table-bordered'>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {customers.map((c) => (
+            <tr key={c.id}>
+              <td>{c.id}</td>
+              <td>{c.firstName}</td>
+              <td>{c.lastName}</td>
+              <td>{c.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 };
